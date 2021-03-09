9a957f010c4c7d18d89059f8dd3e73870bfeab46
zt <ztowo9696@163.cm>
2021-03-09 21:14:34 +0800

    2020.3.9

---
+++ SE/src/day09/homework/Test.java
@@ -0,0 +1,9 @@
+package day09.homework;
+
+public class Test {
+    public static void main(String[] args) {
+        TestSon testSon = new TestSon();
+        TestFather testFather=new TestFather();
+        TestSon.show();
+    }
+}
